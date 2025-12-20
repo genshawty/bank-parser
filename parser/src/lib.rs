5926837f64b3fb0e36a4fa2c69381ec37a839cef
genshawty <mi.kizimov@gmail.com>
2025-12-20 16:03:37 +0300

    fixed bug, added tests

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -26,7 +26,7 @@ impl FromStr for TxType {
         match s.trim().to_ascii_lowercase().as_str() {
             "deposit" => Ok(Self::Deposit),
             "withdrawal" => Ok(Self::Withdrawal),
-            "transfer" => Ok(Self::Withdrawal),
+            "transfer" => Ok(Self::Transfer),
             _ => Err(TxTypeParseError(s.to_string())),
         }
     }
@@ -83,4 +83,126 @@ pub struct Parser {}
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_txtype_from_str_valid() {
+        assert!(matches!(
+            "deposit".parse::<TxType>().unwrap(),
+            TxType::Deposit
+        ));
+        assert!(matches!(
+            "Deposit".parse::<TxType>().unwrap(),
+            TxType::Deposit
+        ));
+        assert!(matches!(
+            "DEPOSIT".parse::<TxType>().unwrap(),
+            TxType::Deposit
+        ));
+        assert!(matches!(
+            "  deposit  ".parse::<TxType>().unwrap(),
+            TxType::Deposit
+        ));
+
+        assert!(matches!(
+            "withdrawal".parse::<TxType>().unwrap(),
+            TxType::Withdrawal
+        ));
+        assert!(matches!(
+            "Withdrawal".parse::<TxType>().unwrap(),
+            TxType::Withdrawal
+        ));
+        assert!(matches!(
+            "WITHDRAWAL".parse::<TxType>().unwrap(),
+            TxType::Withdrawal
+        ));
+
+        assert!(matches!(
+            "transfer".parse::<TxType>().unwrap(),
+            TxType::Transfer
+        ));
+        assert!(matches!(
+            "Transfer".parse::<TxType>().unwrap(),
+            TxType::Transfer
+        ));
+        assert!(matches!(
+            "TRANSFER".parse::<TxType>().unwrap(),
+            TxType::Transfer
+        ));
+    }
+
+    #[test]
+    fn test_txtype_from_str_invalid() {
+        assert!("invalid".parse::<TxType>().is_err());
+        assert!("".parse::<TxType>().is_err());
+        assert!("depos".parse::<TxType>().is_err());
+        assert!("withdrawall".parse::<TxType>().is_err());
+    }
+
+    #[test]
+    fn test_txtype_display() {
+        assert_eq!(TxType::Deposit.to_string(), "DEPOSIT");
+        assert_eq!(TxType::Withdrawal.to_string(), "WITHDRAWAL");
+        assert_eq!(TxType::Transfer.to_string(), "TRANSFER");
+    }
+
+    #[test]
+    fn test_status_from_str_valid() {
+        assert!(matches!(
+            "success".parse::<Status>().unwrap(),
+            Status::Success
+        ));
+        assert!(matches!(
+            "Success".parse::<Status>().unwrap(),
+            Status::Success
+        ));
+        assert!(matches!(
+            "SUCCESS".parse::<Status>().unwrap(),
+            Status::Success
+        ));
+        assert!(matches!(
+            "  success  ".parse::<Status>().unwrap(),
+            Status::Success
+        ));
+
+        assert!(matches!(
+            "failure".parse::<Status>().unwrap(),
+            Status::Failure
+        ));
+        assert!(matches!(
+            "Failure".parse::<Status>().unwrap(),
+            Status::Failure
+        ));
+        assert!(matches!(
+            "FAILURE".parse::<Status>().unwrap(),
+            Status::Failure
+        ));
+
+        assert!(matches!(
+            "pending".parse::<Status>().unwrap(),
+            Status::Pending
+        ));
+        assert!(matches!(
+            "Pending".parse::<Status>().unwrap(),
+            Status::Pending
+        ));
+        assert!(matches!(
+            "PENDING".parse::<Status>().unwrap(),
+            Status::Pending
+        ));
+    }
+
+    #[test]
+    fn test_status_from_str_invalid() {
+        assert!("invalid".parse::<Status>().is_err());
+        assert!("".parse::<Status>().is_err());
+        assert!("sucess".parse::<Status>().is_err());
+        assert!("fail".parse::<Status>().is_err());
+    }
+
+    #[test]
+    fn test_status_display() {
+        assert_eq!(Status::Success.to_string(), "success");
+        assert_eq!(Status::Failure.to_string(), "failure");
+        assert_eq!(Status::Pending.to_string(), "pending");
+    }
 }
